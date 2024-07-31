bc890890a61a26a40974af504916988c2c1806f1
Duzzii <mdundoro@gmail.com>
2024-07-31 15:02:33 +0200

    aaaa

---
+++ chatigo/src/test/java/com/chatiggo/chatigo/ChatigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatiggo.chatigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
