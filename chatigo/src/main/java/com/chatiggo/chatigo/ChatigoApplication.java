2ac15e0df629e1328f6c97ceccefa4be2ee38698
Duzzii <mdundoro@gmail.com>
2024-07-31 15:02:33 +0200

    aaaa

---
+++ chatigo/src/main/java/com/chatiggo/chatigo/ChatigoApplication.java
@@ -0,0 +1,13 @@
+package com.chatiggo.chatigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatigoApplication.class, args);
+	}
+
+}
