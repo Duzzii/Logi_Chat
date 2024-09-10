c51e22919a3b876dc1fcedff7e46b445355fe187
Duzzii <mdundoro@gmail.com>
2024-09-10 15:26:31 +0200

    cors ports changed

--- chatigo/src/main/java/com/chatiggo/chatigo/config/WebConfig.java
+++ chatigo/src/main/java/com/chatiggo/chatigo/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://10.100.0.119:4200")
+                        .allowedOrigins("https://duzzii.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
